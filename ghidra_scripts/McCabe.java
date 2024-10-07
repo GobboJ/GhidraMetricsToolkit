9930d31538198a56d0f9511863bdaf9715d857c0
Jonathan <jonathangobbo@outlook.it>
2024-10-07 17:46:14 +0200

    Added McCabe Cyclomatic complexity implementation

---
+++ ghidra_scripts/McCabe.java
@@ -0,0 +1,116 @@
+//Computes the McCabe cyclomatic complexity of the whole program
+//@author Ca' Foscari - Software Security
+//@category Metrics
+
+import ghidra.app.script.GhidraScript;
+import ghidra.graph.GDirectedGraph;
+import ghidra.graph.GEdge;
+import ghidra.graph.GraphFactory;
+import ghidra.program.model.block.*;
+import ghidra.program.model.listing.Program;
+import ghidra.util.exception.CancelledException;
+import ghidra.util.task.ConsoleTaskMonitor;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class McCabe extends GhidraScript {
+
+    private int countConnectedComponents(GDirectedGraph<String, GEdge<String>> graph) {
+        int count = 0;
+        Set<String> visited = new HashSet<String>();
+        Set<String> todo = new HashSet<>();
+
+        for (String s : graph.getVertices()) {
+
+            if (visited.contains(s)) {
+                continue;
+            }
+
+            visited.add(s);
+            todo.add(s);
+
+            while (!todo.isEmpty()) {
+                Iterator<String> i = todo.iterator();
+                if (i.hasNext()) {
+                    String u = i.next();
+                    visited.add(u);
+                    for (GEdge<String> ss : graph.getIncidentEdges(u)) {
+                        if (!visited.contains(ss.getStart())) {
+                            todo.add(ss.getStart());
+                        }
+                        if (!visited.contains(ss.getEnd())) {
+                            todo.add(ss.getEnd());
+                        }
+                    }
+                    todo.remove(u);
+                }
+            }
+            count += 1;
+        }
+        return count;
+    }
+
+    private int computeMcCabe(Program program) throws CancelledException {
+
+        ConsoleTaskMonitor ctm = new ConsoleTaskMonitor();
+        BasicBlockModel bbm = new BasicBlockModel(program);
+        CodeBlockIterator bIter = bbm.getCodeBlocks(ctm);
+
+        GDirectedGraph<String, GEdge<String>> graph = GraphFactory.createDirectedGraph();
+
+        HashSet<String> vertices = new HashSet<>();
+
+        for (CodeBlock block : bIter) {
+            if (!vertices.contains(block.getName())) {
+                vertices.add(block.getName());
+                graph.addVertex(block.getName());
+            }
+
+            CodeBlockReferenceIterator bDestIter = block.getDestinations(ctm);
+
+            while (bDestIter.hasNext()) {
+                CodeBlockReference ref = bDestIter.next();
+                if (ref.getFlowType().isCall()) {
+                    continue;
+                }
+                String destName = ref.getDestinationBlock().getName();
+                if (vertices.contains(destName)) {
+                    vertices.add(destName);
+                    graph.addVertex(destName);
+                }
+
+                graph.addEdge(new GEdge<>() {
+                    @Override
+                    public String getStart() {
+                        return block.getName();
+                    }
+
+                    @Override
+                    public String getEnd() {
+                        return destName;
+                    }
+                });
+            }
+        }
+
+        int nVertices = graph.getVertexCount();
+        int nEdges = graph.getEdgeCount();
+        int nComponents = countConnectedComponents(graph);
+
+        int result = nEdges - nVertices + 2 * nComponents;
+        printf("Complexity[%s]: %d - %d + 2 * %d = %d\n", program.getName(), nEdges, nVertices, nComponents, result);
+        return result;
+    }
+
+    @Override
+    protected void run() throws Exception {
+        if (currentProgram == null) {
+            printerr("no current program");
+            return;
+        }
+        int complexity = computeMcCabe(currentProgram);
+        println("Complexity: " + complexity);
+    }
+}
